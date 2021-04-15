f4bae9c1e64ed31378f95e765ea610f0e5946d9a
shwetagurnani <shwetaagra28@gmail.com>
2021-04-15 21:08:07 +0530

    Db error fixed

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +15,9 @@ app.use(express.static(path.join(__dirname, "images")));
 
 // const uri = process.env.ATLAS_URI;
 mongoose.connect(
- process.env.MONGODB_URI );
+  // "mongodb+srv://shweta:shweta@cluster0.4lmtv.mongodb.net/ECure?retryWrites=true&w=majority"
+  "mongodb+srv://Shreya:shreya1010@cluster0.4yhlx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
 const connection = mongoose.connection;
 connection.once("open", () => {
@@ -25,9 +27,8 @@ connection.once("open", () => {
 const userRouter = require("./routes/user");
 app.use("/users", userRouter);
 
-if(process.env.NODE_ENV === 'production')
-{
-  app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 app.listen(port, () => {
